0ee1cb860abb42d91fe0ee59e2c266532494e1cc
Jeh-Rodrigues <jessica.rc04@gmail.com>
2025-05-17 23:16:54 -0300

    User entity and resource

---
+++ src/main/java/com/projectcourse/course/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.projectcourse.course.controller;
+
+import com.projectcourse.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "Jessica", "jessica@gmail.com", "45962315", "123456");
+        return ResponseEntity.ok().body(user);
+    }
+}
